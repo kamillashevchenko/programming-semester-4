807a45e910a778367f0066f4b351b86adfe48c10
kamillashevchenko <62769957+kamillashevchenko@users.noreply.github.com>
2020-05-21 14:02:47 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,25 +13,25 @@ int main(int argc, char** argv) {
     struct forth forth = {0};
     forth_init(&forth, stdin, MAX_DATA, MAX_STACK, MAX_RETURN);
     words_add(&forth);
-    if(argc != 1) {
-        for(int i = 1; i < argc; i++){
-            file = fopen(argv[i],"r");
-            if(file) {
-                forth.input = file;
-                forth_run(&forth);
-            }
-            else{
-                if (i == argc - 1 && strcmp(argv[argc - 1], "-") == 0) {
-                    forth.input = stdin;
-                    forth_run(&forth);
-                }
-                printf("Can not read file %s\n", argv[i]);
-                forth_free(&forth);
-                return -1;
-            }
+    for(int i = 1; i < argc; i++){
+        file = fopen(argv[i],"r");
+        if(file) {
+            forth.input = file;
+            forth_run(&forth);
         }
-    }else{
-        forth_run(&forth);
+        else{
+            if(i == argc - 1 && strcmp(argv[argc - 1], "-") == 0) {
+                forth.input = stdin;
+                forth_run(&forth);
+                continue;
+            }
+            printf("Can not read file %s\n", argv[i]);
+            forth_free(&forth);
+            return -1;
+        }
+    }
+    if(argc == 1) {
+        forth_run(&forth);   
     }
     forth_free(&forth);
     return 0;
